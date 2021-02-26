61ea04604ebb63570bf2106fa364d80ad27870cc
Dan Tra <dltra@fcps.edu>
2021-02-26 14:52:51 -0500

    Create ViewPager, Create Fragments as needed, Add to TabLayout on each Fragment creation

--- app/src/main/java/com/example/viewpager2/MainActivity.java
+++ app/src/main/java/com/example/viewpager2/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.viewpager2;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
+public class MainActivity extends AppCompatActivity {
+    ViewPager2 mViewPager;
+    RecyclerView.Adapter mMyFragmentStateAdapter;
+    final int NUM_ITEMS = 5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //assign the instance of ViewPager
+        mViewPager = findViewById(R.id.container);
+
+        //create an adapter for the ViewPager
+        mMyFragmentStateAdapter = new MyFragmentStateAdapter(this);
+
+        //set the adapter for the ViewPager
+        mViewPager.setAdapter(mMyFragmentStateAdapter);
+    }
+    public class MyFragmentStateAdapter extends FragmentStateAdapter{
+
+        public MyFragmentStateAdapter(@NonNull FragmentActivity fragmentActivity) {
+            super(fragmentActivity);
+        }
+        @NonNull
+        @Override
+        public Fragment createFragment(int position) {
+            //return a new instance of MainFragment
+            return MainFragment.newInstance(mViewPager);
+        }
+        @Override
+        public int getItemCount() {
+            return NUM_ITEMS;//number of objects in ViewPager
+        }
     }
 }
